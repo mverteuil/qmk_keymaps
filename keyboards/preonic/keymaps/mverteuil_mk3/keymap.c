8ff08d31e928f3272c68c0e0ffb828be0ce0f352
M. de Verteuil <mverteuil@users.noreply.github.com>
2025-08-04 23:55:24 -0400

    Replace tap dance slash/question with separate keys in MK3 layout

--- keyboards/preonic/keymaps/mverteuil_mk3/keymap.c
+++ keyboards/preonic/keymaps/mverteuil_mk3/keymap.c
@@ -30,8 +30,6 @@
 #define TD_RBRK TD(TD_BRACKETS_RIGHT)
 #define TD_PLEQ TD(TD_PLUS_EQUALS)
 #define TD_SCOL TD(TD_SEMICOLON_COLON)
-#define TD_SLQU TD(TD_SLASH_QUESTION)
-#define TD_SQDQ TD(TD_QUOTE_DOUBLEQUOTE)
 #define TD_USMI TD(TD_UNDERSCORE_MINUS)
 
 /* Sentinel value for invalid tap dance exit */
@@ -57,9 +55,7 @@ enum tapdance_keycodes {
     TD_BRACKETS_LEFT,
     TD_BRACKETS_RIGHT,
     TD_PLUS_EQUALS,
-    TD_QUOTE_DOUBLEQUOTE,
     TD_SEMICOLON_COLON,
-    TD_SLASH_QUESTION,
     TD_UNDERSCORE_MINUS,
 };
 
@@ -92,12 +88,8 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_BRACKETS_RIGHT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_brackets_right_finished, td_brackets_right_reset),
     /* Tap once for plus, twice for equals */
     [TD_PLUS_EQUALS] = ACTION_TAP_DANCE_DOUBLE(KC_PLUS, KC_EQUAL),
-    /* Tap once for single quote, twice for double quote */
-    [TD_QUOTE_DOUBLEQUOTE] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_DOUBLE_QUOTE),
     /* Tap once for semicolon, twice for colon */
     [TD_SEMICOLON_COLON] = ACTION_TAP_DANCE_DOUBLE(KC_SEMICOLON, KC_COLON),
-    /* Tap once for slash, twice for question mark */
-    [TD_SLASH_QUESTION] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_QUESTION),
     /* Tap once for underscore, twice for minus */
     [TD_UNDERSCORE_MINUS] = ACTION_TAP_DANCE_DOUBLE(KC_UNDERSCORE, KC_MINUS),
 };
@@ -109,20 +101,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp    |
  * |------+------+------+------+------+------+------+------+------+------+------+---------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | '  "    |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   '    |
  * |------+------+------+------+------+-------------+------+------+------+------+---------|
  * |CtlEsc|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ;  : | Enter   |
  * |------+------+------+------+------+------|------+------+------+------+------+---------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  | /  ? | Shift   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift   |
  * |------+------+------+------+------+------+------+------+------+------+------+---------|
  * |Numpad| Ctrl | Alt  | GUI  |Lower |Space/Lower |Raise/Enter| Left | Down |  Up  | Right|
  * `---------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_1x2uC (
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_SQDQ,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
   MT_CESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD_SCOL, LT_RSRT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  TD_SLQU, KC_RSFT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   MO_NUMS, KC_LCTL, KC_LALT, KC_LGUI, LOWER,        LT_LWSP,     RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -151,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      | Vol+ |      | Mute |      |      |      |      |      |      |      |
+ * |      |      | Vol+ |      | Mute |      |      |      |      |      |      |   ?  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | Prev | Vol- | Next |Play/P|      |      |   _  |   +  |   -  |   =  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -162,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_1x2uC (
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  _______, _______, KC_VOLU, _______, KC_MUTE, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_VOLU, _______, KC_MUTE, _______, _______, _______, _______, _______, _______, KC_QUES,
   _______, KC_MPRV, KC_VOLD, KC_MNXT, KC_MPLY, _______, _______, KC_UNDS, KC_PLUS, KC_MINS, KC_EQL,  _______,
   _______, _______, _______, _______, KC_MSTP, _______, _______, _______, _______, _______, KC_BSLS, _______,
   _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
@@ -172,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | F11  | F12  | F13  |      |      |      |      |      |      |      |      |
+ * |      | F11  | F12  | F13  |      |      |      |      |      |      |      |   "  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | [ { (| ) } ]|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -183,7 +175,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_1x2uC (
   KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-  _______, KC_F11,  KC_F12,  KC_F13,  _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_F11,  KC_F12,  KC_F13,  _______, _______, _______, _______, _______, _______, _______, KC_DQT,
   _______, _______, _______, _______, _______, _______, _______, TD_LBRK, TD_RBRK, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, _______,
   _______, _______, _______, _______, _______,      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
